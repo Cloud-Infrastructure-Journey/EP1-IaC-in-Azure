ad3eb55a85a88cb326035dfea651bdd56725b812
Kasun Rajapakse <kasunsjc@hotmail.com>
2023-03-20 21:17:25 +0100

    add powershell script

--- scripts/deployment-script.ps1
+++ scripts/deployment-script.ps1
@@ -2,3 +2,7 @@
 
 New-AzResourceGroup -Name rg-bicep -Location westeurope
 
+# create storage account
+
+New-AzResourceGroupDeployment -Name deploy-bicep -ResourceGroupName rg-bicep -TemplateFile .\main.bicep -TemplateParameterFile .\param.json
+
